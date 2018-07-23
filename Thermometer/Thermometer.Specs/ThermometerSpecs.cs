3094ef56389c104496241414a3f7e8cbeb94413a
Hao Wang <haowang@skight.ca>
2018-07-23 10:44:57 -0700

    Add freezing

--- Thermometer/Thermometer.Specs/ThermometerSpecs.cs
+++ Thermometer/Thermometer.Specs/ThermometerSpecs.cs
@@ -14,5 +14,15 @@ namespace Thermometer.Specs
             var notifications =Enumerable.Range(1, 10).Select(x=>(decimal)x).iterate().ToList();
             notifications.Count().ShouldEqual(10);
         };
+
+        private It freezing_iterator_should_send_notification_on_0 = () =>
+        {
+            var notifications = new decimal[]{2,1,0,-1,-2,1,0,-3}.Select(x => (decimal)x).check_freezing().ToList();
+            notifications.Count().ShouldEqual(2);
+            notifications[0].Message.ShouldEqual("Freezing");
+            notifications[1].Message.ShouldEqual("Freezing");
+        };
+
+       
     }
 }
--- Thermometer/Thermometer/Processing.cs
+++ Thermometer/Thermometer/Processing.cs
@@ -13,5 +13,16 @@ namespace Thermometer
                 yield return new Notification{Message = item.ToString(),CreatedDateTime = DateTime.Now};
             }
         }
+
+        public static IEnumerable<Notification> check_freezing(this IEnumerable<decimal> source)
+        {
+            foreach (var item in source)
+            {
+                if (item == 0)
+                {
+                    yield return new Notification { Message = "Freezing", CreatedDateTime = DateTime.Now };
+                }
+            }
+        }
     }
 }
